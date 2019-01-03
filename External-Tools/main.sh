b98aec09c8f428091455be4e90943bd64f9edff2
Zackptg5 <zackptg5@comcast.net>
2019-01-02 22:53:43 -0500

    Fix External Addons

--- External-Tools/main.sh
+++ External-Tools/main.sh
@@ -2,5 +2,5 @@
 
 [ -f $INSTALLER/addon/External-Tools/tools.tar.xz ] && tar -xf $INSTALLER/addon/External-Tools/tools.tar.xz -C $INSTALLER/addon/External-Tools 2>/dev/null
 chmod -R 0755 $INSTALLER/addon/External-Tools
-mv -n $INSTALLER/addon/External-Tools/tools $INSTALLER/common/unityfiles/tools
+cp -R $INSTALLER/addon/External-Tools/tools $INSTALLER/common/unityfiles 2>/dev/null
 PATH=$INSTALLER/common/unityfiles/tools/other:$PATH
