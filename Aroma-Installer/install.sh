7f889bfcd151148f57150456bbc547d737018763
Zackptg5 <zackptg5@comcast.net>
2019-01-15 12:26:07 -0500

    Fix aroma for newer devices without cache

--- Aroma-Installer/install.sh
+++ Aroma-Installer/install.sh
@@ -1,32 +1,34 @@
 # Aroma Installer
 
-if [ -d "/cache/$MODID" ]; then
+[ -L /cache ] && CACHEDIR=/data/cache || CACHEDIR=/cache
+
+if [ -d "$CACHEDIR/$MODID" ]; then
   ui_print "   Continuing install with aroma options"
   # Save selections to Mod
-  for i in /cache/$MODID/*.prop; do
+  for i in $CACHEDIR/$MODID/*.prop; do
     cp_ch -n $i $UNITY/system/etc/$MODID/$(basename $i)
   done
-  rm -f /cache/$MODID.zip /cache/$MODID-Aroma.zip /cache/recovery/openrecoveryscript
-  rm -rf /cache/$MODID
+  rm -f $CACHEDIR/$MODID.zip $CACHEDIR/$MODID-Aroma.zip $CACHEDIR/recovery/openrecoveryscript
+  rm -rf $CACHEDIR/$MODID
 else
   # Delete space hogging boot_log folder
-  rm -rf /cache/boot_log
+  rm -rf $CACHEDIR/boot_log
   if [ -d "$TMPDIR/aroma" ]; then
     # Move previous selections to temp directory for reuse if chosen
     ui_print "   Backup up previous selections..."
     for FILE in $TMPDIR/aroma/*.prop; do
-      cp_ch -i $FILE /cache/$MODID/$(basename $FILE)
+      cp_ch -i $FILE $CACHEDIR/$MODID/$(basename $FILE)
     done
   fi
   ui_print "   Creating Aroma installer and open recovery script..."
-  cp -f $ZIPFILE /cache/$MODID.zip
+  cp -f $ZIPFILE $CACHEDIR/$MODID.zip
   cd $INSTALLER/addon/Aroma-Installer
-  sed -i "2i MODID=$MODID" META-INF/com/google/android/update-binary-installer
+  sed -i -e "2i MODID=$MODID" -e "2i CACHEDIR=$CACHEDIR" META-INF/com/google/android/update-binary-installer
   chmod -R 0755 tools
   cp -R tools $INSTALLER/common/unityfiles 2>/dev/null
-  zip -qr0 /cache/$MODID-Aroma META-INF
+  zip -qr0 $CACHEDIR/$MODID-Aroma META-INF
   cd /
-  echo -e "install /cache/$MODID-Aroma.zip\ninstall /cache/$MODID.zip\nreboot recovery" > /cache/recovery/openrecoveryscript
+  echo -e "install $CACHEDIR/$MODID-Aroma.zip\ninstall $CACHEDIR/$MODID.zip\nreboot recovery" > $CACHEDIR/recovery/openrecoveryscript
   ui_print "   Will reboot and launch aroma installer"
   cleanup
   sleep 3
